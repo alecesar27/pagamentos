92647418264557fb97864cbb1723226ca292c20a
alecesar27 <147946872+alecesar27@users.noreply.github.com>
2024-05-05 12:00:08 -0300

    Update __init__.py

--- webhook/__init__.py
+++ webhook/__init__.py
@@ -8,13 +8,14 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
+
+
+#if os.getenv('DATABASE_URL'):
+#    app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
+#else:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webhook.db'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webhook.db'
 
 from webhook import routes
