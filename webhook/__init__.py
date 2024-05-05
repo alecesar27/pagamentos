fcfdc54885930a7c8600196727ea8458bbbfbbb4
alecesar27 <147946872+alecesar27@users.noreply.github.com>
2024-05-05 11:50:46 -0300

    Update __init__.py

--- webhook/__init__.py
+++ webhook/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-from webhook import models
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
@@ -16,16 +16,5 @@ if os.getenv('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webhook.db'
-
-engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector =sqlalchemy.inspect(engine)
-if not inspector.has_table("usuario"):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print("Base de dados criada!")
-else:
-    print("Base de dados já existente!")
-
 
 from webhook import routes
